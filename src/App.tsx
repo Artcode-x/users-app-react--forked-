aad4bc4e006b0340ddfac1e1ab8fb4cc43f0b1d2
Alex <aironer@yandex.ru>
2024-08-20 02:51:28 +0300

    Отображение списка пользователей и обработка ошибки

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import { requestUsers, requestUsersWithError, User, Query } from "./api";
-import "./styles.css";
-
-import Requirements from "./Requirements";
-
-// Примеры вызова функций, в консоли можно увидеть возвращаемые результаты
-requestUsers({ name: "", age: "", limit: 4, offset: 0 }).then(console.log);
-requestUsersWithError({ name: "", age: "", limit: 4, offset: 0 }).catch(
-  console.error
-);
+import React, { useEffect, useState } from "react"
+import { requestUsers, requestUsersWithError, User } from "./api"
+import "./styles.css"
+import Requirements from "./Requirements"
 
 export default function App() {
-  return <Requirements />;
+  const [users, setUsers] = useState<User[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
+
+  console.log(error)
+  useEffect(() => {
+    requestUsers({ name: "", age: "", limit: 4, offset: 0 })
+      .then((data) => {
+        setUsers(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        requestUsersWithError().catch((someError) => {
+          console.log(someError)
+          setError(someError)
+          setLoading(false)
+        })
+
+        setError(err)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error?.message}</div>
+  }
+
+  return (
+    <div>
+      <Requirements />
+      <h2>Users List:</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{`${user.name}, ${user.age}`}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
