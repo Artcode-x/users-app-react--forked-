37dfc3655bd101df10ef6094034cb093390b5268
Alex <aironer@yandex.ru>
2024-08-20 02:54:02 +0300

    Реализация фильтра по имени

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,52 @@
 import React, { useEffect, useState } from "react"
 import { requestUsers, requestUsersWithError, User } from "./api"
 import "./styles.css"
+
 import Requirements from "./Requirements"
 
 export default function App() {
   const [users, setUsers] = useState<User[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<Error | null>(null)
+  const [nameFilter, setNameFilter] = useState<string>("")
 
-  console.log(error)
-  useEffect(() => {
-    requestUsers({ name: "", age: "", limit: 4, offset: 0 })
+  const fetchUsers = () => {
+    requestUsers({ name: nameFilter, age: "", limit: 4, offset: 0 })
       .then((data) => {
         setUsers(data)
         setLoading(false)
       })
       .catch((err) => {
-        requestUsersWithError().catch((someError) => {
-          console.log(someError)
-          setError(someError)
+        requestUsersWithError().catch((errorWithError) => {
+          setError(errorWithError)
           setLoading(false)
         })
 
         setError(err)
         setLoading(false)
       })
-  }, [])
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [nameFilter])
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNameFilter(event.target.value)
+  }
 
   if (loading) {
     return <div>Loading...</div>
   }
 
   if (error) {
-    return <div>Error: {error?.message}</div>
+    return <div>Error: {error.message}</div>
   }
 
   return (
     <div>
       <Requirements />
+      <input type="text" value={nameFilter} onChange={handleNameChange} placeholder="Фильтр по имени" />
       <h2>Users List:</h2>
       <ul>
         {users.map((user) => (
