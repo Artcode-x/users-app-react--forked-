221525160bf0d50aa2c90ac744c7e63b8a5f9b34
Alex <aironer@yandex.ru>
2024-08-20 03:00:55 +0300

    Реализация пагинации, переключение кол-ва отображения юзеров на странице

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import React, { useEffect, useState } from "react"
 import { requestUsers, requestUsersWithError, User } from "./api"
 import "./styles.css"
 import Requirements from "./Requirements"
-
 export default function App() {
   const [users, setUsers] = useState<User[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState<Error | null>(null)
   const [nameFilter, setNameFilter] = useState<string>("")
   const [ageFilter, setAgeFilter] = useState<string>("")
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [itemsPerPage, setItemsPerPage] = useState<number>(4)
 
   const fetchUsers = () => {
-    requestUsers({ name: nameFilter, age: ageFilter, limit: 4, offset: 0 })
+    const offset = (currentPage - 1) * itemsPerPage
+    requestUsers({ name: nameFilter, age: ageFilter, limit: itemsPerPage, offset })
       .then((data) => {
         setUsers(data)
         setLoading(false)
       })
       .catch((err) => {
-        requestUsersWithError().catch((someError) => {
-          setError(someError)
+        requestUsersWithError().catch((errorWithError) => {
+          setError(errorWithError)
           setLoading(false)
         })
 
@@ -29,14 +31,24 @@ export default function App() {
 
   useEffect(() => {
     fetchUsers()
-  }, [nameFilter, ageFilter])
+  }, [nameFilter, ageFilter, currentPage, itemsPerPage])
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNameFilter(event.target.value)
+    setCurrentPage(1)
   }
 
   const handleAgeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAgeFilter(event.target.value)
+  }
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage)
+  }
+
+  const handleItemsPerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setItemsPerPage(Number(event.target.value))
+    setCurrentPage(1)
   }
 
   if (loading) {
@@ -58,12 +70,36 @@ export default function App() {
         placeholder="Фильтр по имени"
       />
       <input type="text" value={ageFilter} onChange={handleAgeFilter} placeholder="Фильтр по возрасту" />
+
       <h2>Users List:</h2>
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>{`${user.name}, ${user.age}`}</li>
-        ))}
+        {users.length === 0 ? (
+          <li>Users not found</li>
+        ) : (
+          users.map((user) => <li key={user.id}>{`${user.name}, ${user.age}`}</li>)
+        )}
       </ul>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center", paddingRight: "10px" }}>
+          <ul style={{ marginRight: "10px" }}>By Page:</ul>
+          <select onChange={handleItemsPerPageChange} value={itemsPerPage}>
+            <option value={4}>4</option>
+            <option value={8}>8</option>
+            <option value={12}>12</option>
+            <option value={16}>16</option>
+          </select>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <button
+            onClick={() => handlePageChange(currentPage > 1 ? currentPage - 1 : 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+          <span> Page {currentPage} </span>
+          <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+        </div>
+      </div>
     </div>
   )
 }
